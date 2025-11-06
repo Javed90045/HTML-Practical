e6813585f20651fce8d10999a28b9eead5051886
Javed90045 <Javedali90045@gmail.com>
2025-11-06 20:57:27 +0530

    added javascript stuff

---
+++ Javascript/conditional.js
@@ -0,0 +1,28 @@
+console.log("*****************Conditional Statement*****************");
+console.log("*****************IF Statement*****************");
+let x = 10;
+if(x>5)
+{
+    console.log("x is greater...");
+}
+
+console.log("*****************IF-else Statement*****************");
+if(x>10)
+{
+    console.log("x is greater...");
+}
+else{
+    console.log("x is smaller ")
+}
+console.log("*****************Else-IF Statement*****************");
+if(x>10)
+{
+    console.log("x is greater than 10");
+}
+else if(x<=10)
+{
+    console.log("x is smaller than 10");
+}
+else{
+    console.log("invalid number")
+}
