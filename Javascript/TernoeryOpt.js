b718a932a72a5c27de18526dfb3124d9da614a6a
Javed Ali <Javedali90045@gmail.com>
2025-11-14 20:27:39 +0530

    added object

--- Javascript/TernoeryOpt.js
+++ Javascript/TernoeryOpt.js
@@ -1,7 +1,21 @@
 console.log("Ternary Operator");
+
+console.log("WAP to check number is positive or negative");
+
 let x = 0;
 let res = (x>0)?"positive":"negative";
 console.log(res);
 
 res = (x>0)?"positive":(x<0)?"negative":"zero";
+console.log(res);
+
+console.log("WAP to check your grade card");
+
+let marks = 39;
+
+res = (marks>=81 && marks<=100)?"A grade":(marks>=61 && marks<=80)?"B grade":(marks>=40 && marks<=60)?"C grade":"Fail";
+console.log(res);
+
+let n = 39;
+res = (n%2 == 0)?"even":"odd";
 console.log(res);
---
+++ Javascript/object.js
@@ -0,0 +1,15 @@
+console.log("Object is javascript");
+let info = {
+    name:"Mayur",
+    Number: 654654,
+    address: {city:"mumbai", state:"Maharashtra"},
+    addition: function (a,b){
+        return a+b;
+    }
+}
+console.log(info);
+console.log(info.name);
+console.log(info.address.city);
+console.log(info.addition(40,50));
+
+
