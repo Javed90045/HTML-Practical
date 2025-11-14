dce82facfce3810e50c557e38e0c3b03c602fe2b
Javed Ali <Javedali90045@gmail.com>
2025-11-14 20:56:33 +0530

    factory faction

--- Javascript/object.js
+++ Javascript/object.js
@@ -7,9 +7,25 @@ let info = {
         return a+b;
     }
 }
+
 console.log(info);
 console.log(info.name);
 console.log(info.address.city);
 console.log(info.addition(40,50));
+
+function avenger(name, lname){
+    return {
+        f_name: name,
+        l_name: lname
+    }
+}
+
+let a = avenger('steve','roger');
+let a2 = avenger('sau','malhotra');
+
+console.log(a);
+console.log(a2);
+
+
 
 
