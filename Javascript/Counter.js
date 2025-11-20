57e2f596d6ab478fa4a11cf5814de5e00022908b
Javed Ali <Javedali90045@gmail.com>
2025-11-20 20:52:34 +0530

    Made a Counter Project

---
+++ Javascript/Counter.js
@@ -0,0 +1,37 @@
+console.log("Counter Project");
+let display = document.getElementById("display");
+let increase = document.getElementById("inc");
+let reset = document.getElementById("reset");
+let decrease = document.getElementById("desc");
+
+let v = display.value;
+
+function dec() {
+  if (v <= 0) {
+    decrease.setAttribute("disabled", true);
+  } else {
+    decrease.removeAttribute("disabled");
+  }
+}
+
+dec();
+increase.onclick = () => {
+  v++;
+  display.value = v;
+  dec();
+};
+
+decrease.onclick = () => {
+  if (v <= 0) {
+  } else {
+    v--;
+    display.value = v;
+  }
+  dec();
+};
+
+reset.onclick = () => {
+  v = 0;
+  display.value = v;
+  dec();
+};
